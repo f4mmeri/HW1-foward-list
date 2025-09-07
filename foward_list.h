bd067590772c8d6e9533135cfd8f104df1890294
f4mmeri <fabiana.ameri@utec.edu.pe>
2025-09-06 22:34:18 -0500

    add

---
+++ foward_list.h
@@ -0,0 +1,185 @@
+#ifndef FORWARD_LIST_H
+#define FORWARD_LIST_H
+
+#include <iostream>
+using namespace std;
+
+template <class T>
+struct Node {
+    Node* next;
+    T data;
+    Node(T value) : data(value), next(nullptr) {}
+};
+
+template <class T>
+class ForwardList {
+private:
+    Node<T>* head;
+
+public:
+    ForwardList() : head(nullptr) {}
+
+    ~ForwardList() {
+        clear(); // Limpiar los elementos al destruir la lista
+    }
+
+    T front() {
+        if (head != nullptr) {
+            cout << "Front: " << head->data << endl;
+            return head->data;
+        }
+        return T();
+    }
+
+    T back() {
+        if (head == nullptr) return T();
+        Node<T>* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        return temp->data;
+    }
+
+    // Agrega un elemento al comienzo
+    void push_front(T value) {
+        Node<T>* new_node = new Node<T>(value);
+        new_node->next = head;
+        head = new_node;
+    }
+
+    // Agrega un elemento al final
+    void push_back(T value) {
+        Node<T>* new_node = new Node<T>(value);
+        if (head == nullptr) {
+            head = new_node;
+            return;
+        }
+        Node<T>* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+
+    // Elimina el primer elemento
+    T pop_front() {
+        if (head == nullptr) return T();
+        Node<T>* temp = head;
+        T data = temp->data;
+        head = head->next;
+        delete temp;
+        cout << "Pop Front: " << data << endl;
+        return data;
+    }
+
+    // Elimina el último elemento
+    T pop_back() {
+        if (head == nullptr) return T();
+        if (head->next == nullptr) {
+            T data = head->data;
+            delete head;
+            head = nullptr;
+            cout << "Pop Back: " << data << endl;
+            return data;
+        }
+        Node<T>* temp = head;
+        while (temp->next->next != nullptr) {
+            temp = temp->next;
+        }
+        T data = temp->next->data;
+        delete temp->next;
+        temp->next = nullptr;
+        cout << "Pop Back: " << data << endl;
+        return data;
+    }
+
+    // Retorna el elemento en la posición indicada
+    T operator[](int index) {
+        if (head == nullptr || index < 0) return T();
+        Node<T>* temp = head;
+        int counter = 0;
+        while (temp != nullptr) {
+            if (counter == index) {
+                cout << "Element at index " << index << ": " << temp->data << endl;
+                return temp->data;
+            }
+            counter++;
+            temp = temp->next;
+        }
+        return T(); // Si el índice es fuera de rango
+    }
+
+    // Verifica si la lista está vacía
+    bool empty() {
+        bool isEmpty = (head == nullptr);
+        cout << "Is Empty: " << boolalpha << isEmpty << endl;
+        return isEmpty;
+    }
+
+    // Retorna el tamaño de la lista
+    int size() {
+        int count = 0;
+        Node<T>* temp = head;
+        while (temp != nullptr) {
+            count++;
+            temp = temp->next;
+        }
+        cout << "Size: " << count << endl;
+        return count;
+    }
+
+    // Elimina todos los elementos de la lista
+    void clear() {
+        while (head != nullptr) {
+            pop_front();
+        }
+    }
+
+    // Ordena la lista usando un algoritmo simple (burbuja)
+    void sort() {
+        if (head == nullptr) return;
+        Node<T>* temp1 = head;
+        while (temp1 != nullptr) {
+            Node<T>* temp2 = temp1->next;
+            while (temp2 != nullptr) {
+                if (temp1->data > temp2->data) {
+                    T temp = temp1->data;
+                    temp1->data = temp2->data;
+                    temp2->data = temp;
+                }
+                temp2 = temp2->next;
+            }
+            temp1 = temp1->next;
+        }
+    }
+
+    // Revierte el orden de los elementos de la lista
+    void reverse() {
+        Node<T>* prev = nullptr;
+        Node<T>* curr = head;
+        Node<T>* next = nullptr;
+        while (curr != nullptr) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+    // Imprime los elementos de la lista
+    void print() {
+        if (head == nullptr) {
+            cout << "List is empty" << endl;
+            return;
+        }
+        Node<T>* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+#endif // FORWARD_LIST_H
---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "foward_list.h"
+using namespace std;
+
+int main() {
+    cout << "FowardList tests:" << endl;
+    ForwardList<int> Listagod;
+    Listagod.push_front(7);
+    Listagod.push_front(1);
+    Listagod.push_front(3);
+    Listagod.push_front(6);
+    Listagod.push_front(4);
+    Listagod.push_front(5);
+    Listagod.print();
+
+    Listagod.front();
+    Listagod.back();
+    Listagod.push_back(12);
+    Listagod.push_back(2);
+    Listagod.push_back(8);
+    Listagod.pop_back();
+    Listagod.pop_front();
+    Listagod.print();
+
+    Listagod[3];
+    Listagod.empty();
+    Listagod.size();
+    Listagod.sort();
+    Listagod.print();
+
+    Listagod.reverse();
+    Listagod.print();
+    Listagod.clear();
+    Listagod.print();
+}
